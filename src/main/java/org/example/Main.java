3d2473f08244b019a52e3eb61033ad4de4dd77ca
Daniil Dumshenko <dumshenko2003@gmail.com>
2024-10-17 00:50:27 +0200

    first part of homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,11 @@
 package org.example;
 public class Main {
+    public static void main(String[] args) {
+        int [] array1 = {12, 234, 56, 789, 930475389};
+        for (int j : array1) {
+            if ((j % 2) == 0) {
+                System.out.println(j);
+            }
+        }
+    }
 }
